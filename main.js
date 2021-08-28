aef0d750fb86c91d2cec6b423a3bbaaf3ede3bc2
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-08-28 12:32:22 -0600

    refactored, added total loot and feast

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let loot = 0
+let lootCollected = 0
+let winningLoot = 1000
+
 let clickUpgrades ={
   extraLootBags: {
     name: 'Extra Loot Bags',
@@ -33,20 +36,23 @@ let passiveUpgrades = {
 function pillage(){
   if(clickUpgrades.extraLootBags.quantity === 0 && clickUpgrades.lootCarts.quantity === 0){
     loot++
+    lootCollected++
   }
   if(clickUpgrades.extraLootBags.quantity > 0){
     loot += (clickUpgrades.extraLootBags.quantity*clickUpgrades.extraLootBags.multiplier+1)
+    lootCollected += (clickUpgrades.extraLootBags.quantity*clickUpgrades.extraLootBags.multiplier+1)
   }
   if(clickUpgrades.lootCarts.quantity > 0){
     loot += (clickUpgrades.lootCarts.quantity*clickUpgrades.lootCarts.multiplier+1)
+    lootCollected += (clickUpgrades.lootCarts.quantity*clickUpgrades.lootCarts.multiplier+1)
   }
   update()
 }
 
 function villageSally(){
-  let villageClickAttempt = Math.round(Math.random()*10)
-  let villagePassiveAttempt = Math.round(Math.random()*10)
-  if(villageClickAttempt >= 9 && clickUpgrades.extraLootBags.quantity > 0 && clickUpgrades.lootCarts.quantity > 0){
+  let villageClickAttempt = Math.round(Math.random()*100)
+  let villagePassiveAttempt = Math.round(Math.random()*100)
+  if(villageClickAttempt >= 95 && clickUpgrades.extraLootBags.quantity > 0 && clickUpgrades.lootCarts.quantity > 0){
     for(let key in clickUpgrades){
       clickUpgrades[key].quantity--
     }
@@ -54,16 +60,17 @@ function villageSally(){
     clickUpgrades.lootCarts.price = clickUpgrades.lootCarts.price / 2
     // @ts-ignore
     Swal.fire({
-      title: 'Sweet!',
-      text: 'Modal with a custom image.',
+      title: 'The Village defenders have sallied forth!',
+      text: "You've lost 1 of each click item.",
       imageUrl: 'https://www.wallpapertip.com/wmimgs/27-279914_medieval-knight-wallpaper-hd-knights-fight-wallpaper-fantasy.jpg',
       imageWidth: 400,
       imageHeight: 250,
       imageAlt: 'Custom image',
       background: 'black',
+      timerProgressBar: true,
     })
   }
-  if(villagePassiveAttempt >= 9 && passiveUpgrades.sneakThieves.quantity > 0 && passiveUpgrades.vikingHordes.quantity > 0){
+  if(villagePassiveAttempt >= 95 && passiveUpgrades.sneakThieves.quantity > 0 && passiveUpgrades.vikingHordes.quantity > 0){
     for(let key in passiveUpgrades){
       passiveUpgrades[key].quantity--
     }
@@ -71,49 +78,92 @@ function villageSally(){
     passiveUpgrades.vikingHordes.price = passiveUpgrades.vikingHordes.price / 2
     // @ts-ignore
     Swal.fire({
-      title: 'Sweet!',
-      text: 'Modal with a custom image.',
+      title: 'The Village has hired some sellswords!',
+      text: "You've lost 1 of each passive item.",
       imageUrl: 'https://i.pinimg.com/474x/aa/fe/b6/aafeb6ece4bd57f893a39b6dc1d1d059.jpg',
       imageWidth: 400,
       imageHeight: 250,
       imageAlt: 'Custom image',
-      background: 'black'
+      background: 'black',
+      timerProgressBar: true,
     })
   }
   update()
 }
 
-function buyExtraLootBags(){
-if(loot >= clickUpgrades.extraLootBags.price){
-  clickUpgrades.extraLootBags.quantity++
-  loot -= clickUpgrades.extraLootBags.price
-  clickUpgrades.extraLootBags.price = clickUpgrades.extraLootBags.price*2
+function buyClickItems(item){
+if(loot >= clickUpgrades[item].price){
+  clickUpgrades[item].quantity++
+  loot -= clickUpgrades[item].price
+  clickUpgrades[item].price = clickUpgrades[item].price*2
   }
   update()
 }
-function buyLootCarts(){
-if(loot >= clickUpgrades.lootCarts.price){
-  clickUpgrades.lootCarts.quantity++
-  loot -= clickUpgrades.lootCarts.price
-  clickUpgrades.lootCarts.price = clickUpgrades.lootCarts.price*2
+// function buyLootCarts(){
+// if(loot >= clickUpgrades.lootCarts.price){
+//   clickUpgrades.lootCarts.quantity++
+//   loot -= clickUpgrades.lootCarts.price
+//   clickUpgrades.lootCarts.price = clickUpgrades.lootCarts.price*2
+//   }
+//   update()
+// }
+function buyPassiveItems(item){
+if(loot >= passiveUpgrades[item].price){
+  passiveUpgrades[item].quantity++
+  loot -= passiveUpgrades[item].price
+  passiveUpgrades[item].price = passiveUpgrades[item].price*2
   }
   update()
 }
-function buySneakThieves(){
-if(loot >= passiveUpgrades.sneakThieves.price){
-  passiveUpgrades.sneakThieves.quantity++
-  loot -= passiveUpgrades.sneakThieves.price
-  passiveUpgrades.sneakThieves.price = passiveUpgrades.sneakThieves.price*2
+// function buyVikingHordes(){
+// if(loot >= passiveUpgrades.vikingHordes.price){
+//   passiveUpgrades.vikingHordes.quantity++
+//   loot -= passiveUpgrades.vikingHordes.price
+//   passiveUpgrades.vikingHordes.price = passiveUpgrades.vikingHordes.price*2
+//   }
+//   update()
+// }
+
+function hordingHordes(){
+  if(loot >= winningLoot){
+
+    // @ts-ignore
+    Swal.fire({
+      title: 'Congrats, you like win or something.',
+      text: "Now close this tab and go code!",
+      imageUrl: 'https://i.imgflip.com/3oartw.jpg',
+      imageWidth: 400,
+      imageHeight: 500,
+      imageAlt: 'Custom image',
+      background: 'black',
+      timerProgressBar: true,
+    })
+    document.getElementById('vikingFeast').classList.remove('disappear')
   }
-  update()
 }
-function buyVikingHordes(){
-if(loot >= passiveUpgrades.vikingHordes.price){
-  passiveUpgrades.vikingHordes.quantity++
-  loot -= passiveUpgrades.vikingHordes.price
-  passiveUpgrades.vikingHordes.price = passiveUpgrades.vikingHordes.price*2
+
+function vikingFeast(){
+if(loot >= winningLoot){
+  loot = 0
+  for(let key in clickUpgrades){
+    clickUpgrades[key].quantity = 0
   }
-  update()
+  for(let key in passiveUpgrades){
+    passiveUpgrades[key].quantity = 0
+  }
+  document.getElementById('vikingFeast').classList.add('disappear')
+}
+// @ts-ignore
+Swal.fire({
+  title: 'Oh no! A surprise attack!',
+  text: "While everyone was distracted by the festivities the village took all their, i mean all our stuff!",
+  imageUrl: 'https://media.tenor.com/images/f9f2f179b9c88251bd47e4cc90f765c8/tenor.gif',
+  imageWidth: 400,
+  imageHeight: 250,
+  imageAlt: 'Custom image',
+  background: 'black',
+  
+})
 }
 
 function collectAutoUpgrades(){
@@ -122,6 +172,7 @@ function collectAutoUpgrades(){
      passiveTotal += passiveUpgrades[key].multiplier * passiveUpgrades[key].quantity
   }
   loot += passiveTotal
+  lootCollected += passiveTotal
   update()
 }
 
@@ -136,7 +187,7 @@ function preventContext(){
 
 
 function update(){
-  document.getElementById('lootCount').innerText ='Total Loot: ' + loot
+  document.getElementById('lootCount').innerText ='Current Loot: ' + loot
   document.getElementById('extraLootBagsCount').innerText = 'Extra Loot Bags: ' + clickUpgrades.extraLootBags.quantity
   document.getElementById('lootCartsCount').innerText = 'Loot Carts: ' + clickUpgrades.lootCarts.quantity
   document.getElementById('sneakThievesCount').innerText = 'Sneak Thieves: ' + passiveUpgrades.sneakThieves.quantity
@@ -149,14 +200,9 @@ function update(){
      + clickUpgrades.lootCarts.multiplier*clickUpgrades.lootCarts.quantity)
   document.getElementById('passiveCountMultiplier').innerText = 'Passive Multiplier: ' + (passiveUpgrades.sneakThieves.multiplier*passiveUpgrades.sneakThieves.quantity
      + passiveUpgrades.vikingHordes.multiplier*passiveUpgrades.vikingHordes.quantity)
+     document.getElementById('lootCollected').innerText = 'Total Loot Collected: ' + lootCollected 
+  
 }
-
-
-
-
-
-
-
 
 
 update()
